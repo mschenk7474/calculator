75ae61d3d180f9e581862b6b2951d6176dd486ee
mschenk7474 <masonschenk@gmail.com>
2022-10-25 16:57:47 -0600

    updated menu file comments

--- calculator/Main.java
+++ calculator/Main.java
@@ -1,6 +1,12 @@
 package calculator;
 
 public class Main {
+    /*
+     This class is the main class where the program starts and runs from.
+     All of the necessary actions of this program can be found in the
+     menu class, so we create an object of the menu class, and just call
+     the method for which the actual menu resides in.
+     */
     public static void main(String[] args) {
         // menu object intialized and menu method called
         Menu main_menu = new Menu();
